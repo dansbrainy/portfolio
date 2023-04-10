2e8104dd808b903f49e0895c6cbe65c61b36bc41
Daniel <danskamanda@gmail.com>
2023-04-10 21:31:17 +0200

    added geopy on userUpdateView

--- portfolio/users/views.py
+++ portfolio/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.gis.geos import Point
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -19,8 +20,25 @@ user_detail_view = UserDetailView.as_view()
 
 class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = User
-    fields = ["name"]
+    fields = ["name", "home_address", "location"]
     success_message = _("Information successfully updated")
+
+    def form_valid(self, form):
+        # Set the location field based on the home address field
+        address = form.cleaned_data.get("home_address")
+        if address:
+            # Use a geocoding service to get the latitude and longitude of the address
+            # and create a Point object with the coordinates
+            # For example, using geopy:
+            from geopy.geocoders import Nominatim
+
+            geolocator = Nominatim(user_agent="portfolio")
+            location = geolocator.geocode(address)
+            if location:
+                point = Point(location.longitude, location.latitude)
+                form.instance.location = point
+
+        return super().form_valid(form)
 
     def get_success_url(self):
         assert (

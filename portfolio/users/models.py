1fd089f8753be71378da16f77675706be6e16d37
Daniel <danskamanda@gmail.com>
2023-04-10 20:10:36 +0200

    Added profile picture to user model

--- portfolio/users/models.py
+++ portfolio/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.gis.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
 
 class User(AbstractUser):
@@ -20,6 +21,19 @@ class User(AbstractUser):
     home_address = models.CharField(_("Home Address"), blank=True, max_length=255)
     phone_number = models.CharField(_("Phone Number"), blank=True, max_length=20)
     location = models.PointField(_("Location"), blank=True, null=True)
+    profile_picture = models.ImageField(_("Profile Picture"), upload_to="profile_pics/", blank=True, null=True, default='default.jpg')
+
+    def save(self, *args, **kwargs):
+        """Override the save method to resize the profile picture before saving it"""
+        super().save(*args, **kwargs)
+
+        if self.profile_picture:
+            img = Image.open(self.profile_picture.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.profile_picture.path)
 
 
     def get_absolute_url(self):
